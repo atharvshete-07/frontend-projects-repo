e5b4c96eb392844a8ccbdc5b140db8085ae46af7
Atharv Shete <atharvshete1216@gmail.com>
2026-06-21 00:37:57 +0530

    Add files via upload

---
+++ Calculator-app/src/componets/ButtonContainer.jsx
@@ -0,0 +1,11 @@
+import styles from"./buttonContainer.module.css";
+const ButtonContainer=({onButtonClick})=>{
+    const buttonName=['C','%','/','*','7','8','9','-','4','5','6','+','1','2','3','=','0','.'];
+    return(
+       <div className={styles.buttonContainer}>
+        {buttonName.map(buttonName=><button className={styles.button} onClick={()=>onButtonClick(buttonName)}>{buttonName}</button>)}
+    </div>
+    );
+};
+
+export default ButtonContainer;
